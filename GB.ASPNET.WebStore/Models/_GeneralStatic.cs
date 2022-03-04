626dd6baee32af1e44a5761fabdafc5163e97a1d
Dymitr-Szlykow <62315853+Dymitr-Szlykow@users.noreply.github.com>
2022-03-04 20:47:18 +0300

    Занятие 2.
    - использована статическая модель сайта, разделена по частичным представлениям

--- GB.ASPNET.WebStore/Models/_GeneralStatic.cs
+++ GB.ASPNET.WebStore/Models/_GeneralStatic.cs
@@ -4,10 +4,14 @@
     {
         public static string AgeUnit(int age)
         {
-            int lastDigit = age % 10;
-            if (lastDigit == 1) return "год";
-            else if (lastDigit > 1 && lastDigit < 5) return "года";
-            else return "лет";
+            if (10 < age && age < 15) return "лет";
+            else
+            {
+                int lastDigit = age % 10;
+                if (lastDigit == 1) return "год";
+                else if (lastDigit > 1 && lastDigit < 5) return "года";
+                else return "лет";
+            }
         }
     }
 }
--- GB.ASPNET.WebStore/Program.cs
+++ GB.ASPNET.WebStore/Program.cs
@@ -1,13 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
-_ = builder.Services.AddControllersWithViews();
 
 // регистрация сервисов
+_ = builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+_ = app.UseStaticFiles();
 _ = app.UseRouting();
 
-_ = app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
-//_ = app.MapPost("/home/employeeupdate/{id:number}", (id) => )
+//_ = app.MapGet("/greetings", () => app.Configuration["ServerGreetings"]);
+_ = app.MapGet("/throw", handler: () => { throw new ApplicationException("Пример ошибки."); });
+//_ = app.MapPost("/home/employeeupdate/{id:number}", (id) => );
 
 _ = app.MapDefaultControllerRoute();
 //app.MapControllerRoute(
