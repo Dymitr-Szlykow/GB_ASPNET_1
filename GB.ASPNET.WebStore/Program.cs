3f1f99db4a59d30d2f51e54eef5bacf3dac0e8e1
Dymitr-Szlykow <62315853+Dymitr-Szlykow@users.noreply.github.com>
2022-03-23 02:34:36 +0300

    Разнес комманды в program.cs по методам расширения.

--- GB.ASPNET.WebStore/Program.cs
+++ GB.ASPNET.WebStore/Program.cs
@@ -4,33 +4,61 @@ using GB.ASPNET.WebStore.Data;
 using GB.ASPNET.WebStore.Services;
 using GB.ASPNET.WebStore.Services.Interfaces;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// р е г и с т р а ц и я  с е р в и с о в
-_ = builder.Services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
-_ = builder.Services.AddScoped<IProductData, InMemoryProducData>();
-_ = builder.Services.AddControllersWithViews();
-//_ = builder.Services.AddControllersWithViews(opt =>
-//{
-//    opt.Conventions.Clear();
-//    opt.Conventions.Add(new DeletemeConvention());
-//});
+WebApplication
+    .CreateBuilder(args)
 
-var app = builder.Build();
+    .SetMyServices()
+    .Build()
 
-if (app.Environment.IsDevelopment())
-    _ = app.UseDeveloperExceptionPage();
-_ = app.UseStaticFiles();
-_ = app.UseRouting();
-// _ = app.UseMiddleware<DeletemeMiddleware>();  // custom middleware
+    .SetMyMiddlewarePipeline()
+    .MapMyRoutes()
+    .Run();
 
-_ = app.MapGet("/throw", handler: () => { throw new ApplicationException("Пример ошибки."); });
-//_ = app.MapPost("/home/employeeupdate/{id:number}", (id) => );
 
-_ = app.MapDefaultControllerRoute();
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}"
-//);
+public static class WebStoreBuildHelper
+{
+    public static WebApplicationBuilder SetMyServices(this WebApplicationBuilder builder)
+    {
+        _ = builder.Services
+            .AddScoped<IEmployeesData, InMemoryEmployeesData>()
+            .AddScoped<IProductData, InMemoryProducData>();
 
-app.Run();
+        _ = builder.Services.AddControllersWithViews();
+        //_ = builder.Services.AddControllersWithViews(opt =>
+        //{
+        //    opt.Conventions.Clear();
+        //    opt.Conventions.Add(new DeletemeConvention());
+        //});
+
+        return builder;
+    }
+
+    public static WebApplication SetMyMiddlewarePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            _ = app.UseDeveloperExceptionPage();
+        }
+
+        _ = app
+            .UseStaticFiles()
+            .UseRouting();
+            //.UseMiddleware<DeletemeMiddleware>();  // custom middleware
+
+        return app;
+    }
+
+    public static WebApplication MapMyRoutes(this WebApplication app)
+    {
+        _ = app.MapGet("/throw", handler: () => { throw new ApplicationException("РџСЂРёРјРµСЂ РѕС€РёР±РєРё."); });
+
+        _ = app.MapDefaultControllerRoute();
+        //app.MapControllerRoute(
+        //    name: "default",
+        //    pattern: "{controller=Home}/{action=Index}/{id?}"
+        //);
+
+        return app;
+    }
+}
